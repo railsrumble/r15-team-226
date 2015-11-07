472f045a1ea98c99c91a09850f9010e40745ab0d
fazal <fazalvjit81@gmail.com>
2015-11-07 11:35:02 +0530

    added dashboard controller and modified message read default value to false

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,4 @@
 class Owner < ActiveRecord::Base
+  has_many :messages_as_sender, class_name: "Message", foreign_key: :sender_id
+  has_many :messages_as_receiver, class_name: "Message", foreign_key: :receiver_id
 end

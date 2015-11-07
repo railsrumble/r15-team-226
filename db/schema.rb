c073cc5589c4af5bbfc385c58b4b448e985ef20e
rafeeq <rafeeq.sk786@gmail.com>
2015-11-07 11:58:43 +0530

    added :confirmable to owner

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107060348) do
+ActiveRecord::Schema.define(version: 20151107062642) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "attachable_id"
@@ -62,6 +62,10 @@ ActiveRecord::Schema.define(version: 20151107060348) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "owners", ["email"], name: "index_owners_on_email", unique: true

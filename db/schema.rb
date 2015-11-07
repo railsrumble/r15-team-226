7f33025100385a76bce78e5c31a8da349d372ef0
rafeeq <rafeeq.sk786@gmail.com>
2015-11-07 11:43:49 +0530

    fixed devise issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107054414) do
+ActiveRecord::Schema.define(version: 20151107055941) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "attachable_id"
@@ -51,9 +51,21 @@ ActiveRecord::Schema.define(version: 20151107054414) do
     t.string   "last_name"
     t.string   "email"
     t.string   "contact_number"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "owners", ["email"], name: "index_owners_on_email", unique: true
+  add_index "owners", ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
 
   create_table "pets", force: :cascade do |t|
     t.string   "pet_type"

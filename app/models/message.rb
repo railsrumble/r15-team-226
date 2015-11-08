e25df54848e5333aae772bfeba3e382fcc18db7a
Suman Ghosh <suman.ghosh@coppernine.com>
2015-11-09 03:33:19 +0530

    Added walk requests

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,14 @@ class Message < ActiveRecord::Base
   belongs_to :receiver, class_name: 'Owner',:foreign_key => "receiver_id"
   has_many :attachments, :as => :attachable, dependent: :destroy
   accepts_nested_attributes_for :attachments
+
+
+  def self.user_sent_walk_requests(current_owner)
+    where(subject: 'Walk request', sender_id: current_owner.id)
+  end
+
+
+  def self.user_received_walk_requests(current_owner)
+    where(subject: 'Walk request', receiver_id: current_owner.id)
+  end
 end

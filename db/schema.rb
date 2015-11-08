458864183c8882a02b0823b194f6e2c6d561fae0
fazal <fazalvjit81@gmail.com>
2015-11-08 13:35:24 +0530

    pet veiw modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108050642) do
+ActiveRecord::Schema.define(version: 20151108054242) do
 
   create_table "adoption_requests", force: :cascade do |t|
     t.integer  "owner_id"

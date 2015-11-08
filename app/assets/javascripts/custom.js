33d17bd34c409ef7eb9136fe71ae1b6a43881e32
fazal <fazalvjit81@gmail.com>
2015-11-08 16:36:14 +0530

    done messages

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,2 +1,9 @@
 
 $(".alert" ).fadeOut(4000);
+
+// enabling first message from the list
+if ($('.first-message-enable').length) {
+  $(window).load(function() {
+     $('.first-message-enable a:first').click();
+  });
+  }
